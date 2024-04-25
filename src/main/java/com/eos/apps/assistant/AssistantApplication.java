b53468c1d6ca38710c0b6743f034bd5cb8fa476c
Raul Urechiatu <raul@areusdev.com>
2024-04-25 12:18:18 +0300

    First commit

---
+++ src/main/java/com/eos/apps/assistant/AssistantApplication.java
@@ -0,0 +1,13 @@
+package com.eos.apps.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eos/apps/assistant/AssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eos.apps.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
